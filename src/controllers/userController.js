27e65db4be1162c60199f2b44e8ff666e7e78183
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-25 19:59:13 +0100

    Finsih input validation and add success bool to responses

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,33 @@
 const User = require("../models/user");
-
 const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const { username, email, password, dateOfBirth, countryOfOrigin } =
+      req.body;
+
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exists" });
+    }
+
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      username,
+      email,
       password: hashedPassword,
-      dateOfBirth: new Date(req.body.dateOfBirth),
-      countryOfOrigin: req.body.countryOfOrigin,
+      dateOfBirth: new Date(dateOfBirth),
+      countryOfOrigin,
     });
 
     await newUser.save();
@@ -23,12 +39,16 @@ const registerUser = async (req, res) => {
       countryOfOrigin: newUser.countryOfOrigin,
     };
 
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: userForResponse });
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: userForResponse,
+    });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: "Error creating user", error: err });
+    return res
+      .status(500)
+      .json({ success: false, message: "Error creating user", error: err });
   }
 };
 
