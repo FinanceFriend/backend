8e1db2137c1b15e2e2e140b8aeb47a1b44f7ae25
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-25 19:39:30 +0100

    Add password security mesures to registration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,39 @@
 const User = require("../models/user");
 
+const bcrypt = require("bcryptjs");
+
 const registerUser = async (req, res) => {
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password, //todo hash password
+      password: hashedPassword,
       dateOfBirth: new Date(req.body.dateOfBirth),
       countryOfOrigin: req.body.countryOfOrigin,
     });
 
     await newUser.save();
+
+    const userForResponse = {
+      username: newUser.username,
+      email: newUser.email,
+      dateOfBirth: newUser.dateOfBirth,
+      countryOfOrigin: newUser.countryOfOrigin,
+    };
+
     res
       .status(201)
-      .json({ message: "User created successfully", user: newUser }); //todo - not return password to user
+      .json({ message: "User created successfully", user: userForResponse });
   } catch (err) {
-    res.status(500).json({ message: "Error creating user", error: err });
+    console.error(err);
+
+    if (err.name === "ValidationError") {
+      return res.status(400).json({ message: "Invalid input data" });
+    }
+
+    return res.status(500).json({ message: "Error creating user" });
   }
 };
 
