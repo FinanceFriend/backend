32161c338f08a798a99f851cb8327e844d493da4
Karla <karla.smuk@gmail.com>
2023-12-28 16:11:45 +0100

    Changed to work with json files

--- src/langchain/scripts/script.py
+++ src/langchain/scripts/script.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import openai
 from langchain.llms.openai import OpenAI
 from langchain.prompts import PromptTemplate
-import re, pdfplumber, sys, json
+import sys, json
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -11,24 +11,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 lesson_index = int(sys.argv[1])
 mini_lesson_index = int(sys.argv[2])
 
-with pdfplumber.open('src/langchain/docs/Savings.pdf') as pdf:
-    full_text = ''
-    for page in pdf.pages:
-        full_text += page.extract_text()
 
-# Split the text by weeks
-week_pattern = r'Week \d+:'
-weeks = re.split(week_pattern, full_text)[1:]  # Skipping the first split as it's likely empty
+with open('src/langchain/docs/Numberland_converted.json', 'r') as file:
+    lessons = json.load(file)
 
-# Initialize a 2D array for weeks and lessons
-week_lessons = []
-
-for week in weeks:
-    # Extract mini-lessons for each week
-    lesson_pattern = r'Mini-Lesson \d+: (.*?)\n• Content: (.*?)(?=\nMini-Lesson \d+:|\Z)'
-    lessons = re.findall(lesson_pattern, week, re.DOTALL)
-    week_lessons.append(lessons)
-
+lesson = lessons[lesson_index]
+sublesson = lesson['sublessons'][mini_lesson_index]
 
 llm = OpenAI(temperature=0, model_name = 'text-davinci-003')
 
@@ -37,8 +25,7 @@ prompt = PromptTemplate(
     template="Explain the following to a child in simple terms:\n{text}"
 )
 
-lesson_title, lesson_content = week_lessons[lesson_index][mini_lesson_index]
-query = lesson_title + ": " + lesson_content
+query = sublesson['name'] + ' ' + sublesson['goal']
 
 final_prompt = prompt.format(text=query)
 output = llm(final_prompt)
