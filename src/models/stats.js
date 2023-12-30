04da94b5d7dac718708c2595d0794b91755d6cc2
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-30 21:27:46 +0100

    Add stats model

---
+++ src/models/stats.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const statsSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  completionPercentages: {
+    type: [Number],
+    validate: {
+      validator: function (array) {
+        return array.every((num) => num >= 0 && num <= 100);
+      },
+      message: "Completion percentages must be between 0 and 100",
+    },
+    required: true,
+  },
+  points: {
+    type: [Number],
+    validate: {
+      validator: function (array) {
+        return array.every((num) => num >= 0);
+      },
+      message: "Points must be non-negative",
+    },
+    required: true,
+  },
+  correctAnswers: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  incorrectAnswers: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+module.exports = mongoose.model("Stats", statsSchema);
