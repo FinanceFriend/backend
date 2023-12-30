2ea69157dcaaa9292a30b66c9faad6587c3070f7
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-31 00:21:51 +0100

    Implement and document country leaderboard and country single user leaderboard

--- src/routes/leaderboardsRoutes.js
+++ src/routes/leaderboardsRoutes.js
@@ -4,5 +4,7 @@ const leaderboardsController = require('../controllers/leaderboardsController');
 
 router.get('/leaderboard/general', leaderboardsController.getGeneralLeaderboard);
 router.get('/leaderboard/general/:username', leaderboardsController.getGeneralLeaderboardByUser);
+router.get('/leaderboard/:country', leaderboardsController.getCountryLeaderboard);
+router.get('/leaderboard/:country/:username', leaderboardsController.getCountryLeaderboardByUser);
 
 module.exports = router;
